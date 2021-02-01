4e54639a81097c95f7cfdb662651e32ba81299c9
= <c19738789@mytudublin.ie>
2021-02-01 17:07:30 +0000

    Solution

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,34 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+    private int numLives;
     public Cat(String name)
     {
         super(name);
+        numLives = 9;
     }
+
+    public int getNumLives() {
+        return numLives;
+    }
+
+    public void setNumLives(int numLives) {
+        this.numLives = numLives;
+    }
+
+    void kill()
+    {
+        if (numLives > 0)
+        {
+            System.out.println("ouch!");
+            numLives --;
+        }
+        else
+        { 
+            System.out.println("I am dead!");
+        }
+    }
+
+    // You need to put your code here!
+    
 }
