34a5ffc69536f12f543da7037a4d9fb3653dffc2
= <c19738789@mytudublin.ie>
2021-02-04 10:42:45 +0000

    week2

--- java/src/ie/tudublin/HelloProcessing1.java
+++ java/src/ie/tudublin/HelloProcessing1.java
@@ -65,3 +65,4 @@ public class HelloProcessing1 extends PApplet
 		}
 	}
 }
+
--- java/src/ie/tudublin/HelloProcessing2.java
+++ java/src/ie/tudublin/HelloProcessing2.java
@@ -14,10 +14,21 @@ public class HelloProcessing2 extends PApplet
 		
 	}
 	
+	
 	public void draw()
 	{	
-		background(0);
-		stroke(255);
-		line(10, 10, 100, 100);
+		background(255, 0, 0); //black
+		stroke(0, 255, 0); // pen colour
+		
+
+		noStroke();
+		ellipse(250, 300, 330, 345);// cx, cy, w, h
+		fill(153, 1);
+
+		triangle(270, 270, 250, 320, 300, 350);
+		fill(88);
+
+		ellipse(50, 70, 90, 100);// cx, cy, w, h
+		fill(0);
 	}
 }
